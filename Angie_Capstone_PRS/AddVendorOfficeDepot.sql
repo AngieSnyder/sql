6d4796446fe37df0307ebfddcccd9dac011eeed1
AngieSnyder <Angie.Snyder@protonmail.com>
2018-09-01 23:57:35 -0400

    Added Vendor Offic eDepot

---
+++ Angie_Capstone_PRS/AddVendorOfficeDepot.sql
@@ -0,0 +1,3 @@
+INSERT INTO `vendor` (Code, Name, Address, City, State, Zip, PhoneNumber, Email, IsPreApproved) 
+VALUES 
+('OD-1001','Office Depot','6600 North Military Trail','Boca Raton','FL','33496','800-4633768','customersupport@officedepot.com', 1)
